677fc08b3c89aa4bfe9db424946e88b51ee879e5
Elizabeth Thys <elizabeth.thys@gmail.com>
2019-01-30 21:30:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,5 @@ var recipes = {}; /*describe('Objects', function() {
 function updateObjectWithKeyAndValue (object, key, value){
   var obj = { prop: 1 };
   obj.prop2 = 2;
-  obj.prop = 'old value';
-  var newObj = updateObjectWithKeyAndValue(obj, prop, 'new value')
 return obj;
 }
