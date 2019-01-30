66f73ec4753e8440489784e7b4f6d327c66142bb
Elizabeth Thys <elizabeth.thys@gmail.com>
2019-01-30 21:53:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ return Object.assign({}, object, {[key]: value});
 
 function destructivelyUpdateObjectWithKeyAndValue  (object, key, value){
   var obj = { prop: 1 };
+  obj.prop2 = 2;
 return obj;
 }
