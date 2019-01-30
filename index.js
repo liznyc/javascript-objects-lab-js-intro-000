1567780d1a83640dedaf038602c4ebe3a6285433
Elizabeth Thys <elizabeth.thys@gmail.com>
2019-01-30 21:14:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-var recipes = {};
+var recipes = {}; /*describe('Objects', function() {
+  it('defines a `recipes` object', function() {
+    expect(typeof recipes).toEqual('object')
+  })*/
+
+function updateObjectWithKeyAndValue (object, key, value){
+  var obj = { prop: 1 }
+return obj;
+}
