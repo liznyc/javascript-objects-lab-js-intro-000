9f73961e386ed0128dd120280eb0f5884fe2d999
Elizabeth Thys <elizabeth.thys@gmail.com>
2019-01-30 21:52:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ var recipes = new Object();
 function updateObjectWithKeyAndValue (object, key, value){
 return Object.assign({}, object, {[key]: value});
 }
+
+function destructivelyUpdateObjectWithKeyAndValue  (object, key, value){
+  var obj = { prop: 1 };
+  obj.prop2 = 2;
+return obj;
+}
